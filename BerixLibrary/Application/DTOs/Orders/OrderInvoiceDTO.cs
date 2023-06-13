a09132929afb8b79aea5d2eeddb7ab14b67e31d2
Aleksa Berisavac <aleksa-berisavac@live.com>
2023-06-13 15:29:27 +0200

    Dodati fajlovi koji nisu dodati u prethodnom komitu.

---
+++ BerixLibrary/Application/DTOs/Orders/OrderInvoiceDTO.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs.Books;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Orders
+{
+    public class OrderInvoiceDTO
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int OrderId { get; set; }
+        public int NumberOfItems { get; set; }
+        virtual public BookDTO Book { get; set; }
+        virtual public OrderDTO Order { get; set; }
+    }
+}
---
+++ BerixLibrary/Implementation/Profiles/BookProfile.cs
@@ -0,0 +1,53 @@
+﻿using Application.DTOs.Books;
+using Application.DTOs.IntersectEntities;
+using Application.DTOs.UseCases;
+using AutoMapper;
+using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Implementation.Profiles
+{
+    public class BookProfile: Profile
+    {
+        public BookProfile()
+        {
+            CreateMap<BookDTO, Book>()
+               .ForMember(dto => dto.Authors, opt => opt.MapFrom(dto => dto.Authors.Select(x => new BookAuthor
+               {
+                   AuthorId = x.Id,
+                   BookId = dto.Id
+               })))
+               .ForMember(dto => dto.Prices, opt => opt.MapFrom(dto => dto.Prices.Select(x => new BookPrice
+               {
+                   BookId = dto.Id,
+                   Price = x.Price
+               })))
+               .ForMember(dto => dto.Genres, opt => opt.MapFrom(dto => dto.Genres.Select(x => new BookGenre
+               {
+                   GenreId = x.Id,
+                   BookId = dto.Id
+               })));
+
+            CreateMap<Book, BookDTO>()
+               .ForMember(dto => dto.Authors, opt => opt.MapFrom(dto => dto.Authors.Select(x => new BookAuthor
+               {
+                   AuthorId = x.AuthorId,
+                   BookId = dto.Id
+               })))
+               .ForMember(dto => dto.Prices, opt => opt.MapFrom(dto => dto.Prices.Select(x => new BookPrice
+               {
+                   BookId= dto.Id,
+                   Price= x.Price
+               })))
+                .ForMember(dto => dto.Genres, opt => opt.MapFrom(dto => dto.Genres.Select(x => new BookGenre
+                {
+                    GenreId = x.GenreId,
+                    BookId = dto.Id
+                })));
+        }
+    }
+}
